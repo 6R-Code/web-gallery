82ba8f4de13183ada6929a4fe18e7e2e7d40780a
6R-Code <lucascizairegame@gmail.com>
2024-01-30 17:13:36 +0100

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,31 @@ function App() {
 
   return (
     <>
+    
+    <div className="App">
 
+      <header className="App-header">
+        <div className='App-header-left'>
+          <h1>PROJECT 3D</h1>
+        </div>
+        <div className='App-header-right'>
+          <h3>Account</h3>
+          <h3>Product</h3>
+          <h3>Investments</h3>
+          <img src='./image.png' alt='Image'className='App-header-logo'></img>
+        </div>
+      </header>
+    </div>
+
+
+
+
+
+
+
+
+
+    
     </>
   )
 }
