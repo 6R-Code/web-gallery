89eb7ebf0faa9c1a2696dc7055a988458b252847
6R-Code <lucascizairegame@gmail.com>
2024-02-01 17:51:43 +0100

    Importing a logo

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <h3>Account</h3>
           <h3>Product</h3>
           <h3>Investments</h3>
-          <img src='./image.png' alt='Image'className='App-header-logo'></img>
+          <img src='./logo.jpg' alt='Image'className='App-header-logo'></img>
         </div>
       </header>
     </div>
